a64b5c9156e37600bb4a0568dfcce1b8279a0954
arnurkuatov <arnurkuatov@gmail.com>
2019-11-03 21:58:43 +0600

    My first JS git

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+ };
+// console.log(persone["name"])
+
+let arr = ['apple.png', 'orange.jpg', 'banan.bmp'];
+console.log(arr[0]);
